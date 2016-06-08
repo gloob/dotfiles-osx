13c66f47402b4f807b53e881370346408d5ca662
Alejandro Leiva <gloob@litio.org>
2016-06-08 11:12:29 -0400

    Properly installing vim-plug.

--- vim/install.sh
+++ vim/install.sh
@@ -2,6 +2,12 @@
 #
 # vim installation.
 
+# Create .vim/autoload directory if it doesn't exists.
+mkdir -p ~/.vim/autoload
+
 # Install vim-plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# Launch :PlugInstall
+vim -c "PlugInstall | q | q"
