21ebb196c4cc57983eac836c9cdd507205a7f0a8
Alejandro Leiva <gloob@litio.org>
2016-07-19 12:18:13 +0200

    Adding FXShowPosixPathInTitle to osx defaults.

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -39,3 +39,6 @@ defaults write com.apple.Safari IncludeDevelopMenu -bool true
 defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
 defaults write com.apple.Safari "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" -bool true
 defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
+
+# Show full POXIX path in Finder
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
